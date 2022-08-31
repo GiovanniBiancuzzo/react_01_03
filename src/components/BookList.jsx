382022b4bdbc479f5e7c062ab2cb7302becb6c6f
Giovanni Biancuzzo <giovannibiancuzzo@protonmail.com>
2022-08-31 17:49:03 +0200

    commenti

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,7 @@
 // Crea un componente BookList. Questo componente riceverà dalle prop una lista di libri da visualizzare utilizzando il componente SingleBook.
 
+// Crea una funzione filterBookList. Scrivendo una stringa di ricerca dentro un campo input, il componente BookList dovrà renderizzare solo i libri contenente la stringa cercata come titolo del libro (suggerimento: salva la stringa di ricerca dentro allo stato del componente BookList e filtra i libri di conseguenza).
+
 import { Component } from "react";
 import { Col, Container, Form, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,5 +1,7 @@
 // Crea un componente SingleBook e questa volta dovrà essere un componente creato a funzione. Il componente riceve un oggetto corrispondente ad un singolo libro come prop, e visualizzerà la sua copertina e il titolo del libro.
 // Usa le Card di react-bootstrap per renderizzare il libro (l’oggetto del libro può esser letto da un file .json che hai ricevuto ieri).
+// Converti il tuo componente SingleBook in una classe, e crea il suo stato contenente una proprietà booleana selected.
+// Cliccare sul SingleBook dovrà fare il toggle della proprietà selected. Se la proprietà selected sarà true, il SingleBook dovrà ricevere dello stile che rifletta il cambio di stato, visivamente.
 
 import { Component } from "react";
 import { Button, Card } from "react-bootstrap";
